e1afa1f7a605b4fecfb100395d02d08b4f06ad28
Chris DeLeon <gamkedo@gmail.com>
2016-01-20 14:07:39 -0800

    Initial Unity project set up for Gear VR with look+tap input working, will post the video shortly about how this type of project's set up (esp. in case of use to other future teams in the club who may also be VR-interested)

---
+++ Assets/TestSpin.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSpin : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right, 40.0f * Time.deltaTime);	
+	}
+}
