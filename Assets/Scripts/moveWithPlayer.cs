6f34dfc73722b78159f43fe7c9f8db810f29f047
Chris DeLeon <gamkedo@gmail.com>
2016-02-11 03:09:12 -0800

    Added "futuristic" (ahem) looking car models as collateral damage pieces in skyline area, set up player as prefab so that skyline area is playable again with proper ui hookup etc

---
+++ Assets/Scripts/moveWithPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveWithPlayer : MonoBehaviour {
+	public Transform followGO;
+	Vector3 offset;
+
+	void Start() {
+		offset = transform.position - followGO.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = followGO.position + offset;
+	
+	}
+}
