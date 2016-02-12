413e193a7a831b9e8952f46f9362e341770617a4
Chris DeLeon <gamkedo@gmail.com>
2016-02-11 17:32:51 -0800

    Adjustments to camera prefab for better Gear VR compatibility

---
+++ Assets/Scripts/TurnOnCameraIfNonEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnOnCameraIfNonEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera thisCam = GetComponent<Camera>();
+		thisCam.enabled = (Application.isEditor == false);
+	}
+}
